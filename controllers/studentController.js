3cc23e6e8bf3eaa9c149f4d24079e97d761ea9ec
Moneshwar R <moneshwar.r2024it@sece.ac.in>
2025-12-12 22:41:36 +0530

    Commited

---
+++ controllers/studentController.js
@@ -0,0 +1,9 @@
+const Student=require('../models/Student')
+exports.createStudent=async(req,res)=>{
+    try{
+        const student=await Student.create(req.body)
+        res.json(student) 
+    }catch(err){
+        res.status(400).json({error:err.message})
+    }
+}
